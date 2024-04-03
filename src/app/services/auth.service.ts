d2c3d5290b2d4693813c051e7426ce3496ed9a64
Michael Benton-Sanchez <mbs652001@gmail.com>
2024-04-02 23:15:39 -0400

    Updated auth service to identify if the user has been authenticated.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,23 +2,31 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { SignInData } from '../models/signin.interface';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
+  private isAuthenticated: boolean = false;
+
   // Inject AngularFireAuth
-  constructor(private auth: AngularFireAuth) { }
+  constructor(private auth: AngularFireAuth) {}
 
   async login(SignInData: SignInData): Promise<boolean> {
     try {
-      await this.auth.signInWithEmailAndPassword(SignInData.email, SignInData.password);
-      return true;
+      await this.auth.signInWithEmailAndPassword(
+        SignInData.email,
+        SignInData.password
+      );
     } catch (error) {
       return false;
     }
+    this.isAuthenticated = true;
+    return true;
   }
 
+  isLoggedIn() {
+    return this.isAuthenticated;
+  }
 
 }
